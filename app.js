373081c5c215048622cbafc06efbe05eba5f826d
Anus Shafiq <sheikhanus925@gmail.com>
2024-10-20 22:35:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 document.write(`<h1>Find & Replace Game</h1>`);
 
 var para = prompt("Please Enter Your Paragraph");
-document.write(para.toLowerCase() + "<Br /><Br />");
+var paragraph = para.toLowerCase;
+document.write(para + "<Br /><Br />");
 var choose = +prompt(
   "Please type 1 to just find Words type 2 for find & replace the words"
 );
 
 if (choose === 1) {
   var find = prompt("Please enter a word to find");
-  var updatedPara = para.replaceAll(find, ` <u>${find}</u>`);
+  var updatedPara = para.toLowerCase().replaceAll(find, ` <u>${find}</u>`);
 
   document.write(updatedPara);
 } else if (choose === 2) {
   find = prompt("Please enter a word to find");
   var replace = prompt("Please enter a word you want to replace");
-  var updatedPara = para.replaceAll(find, replace);
+  var updatedPara = para.toLowerCase().replaceAll(find, replace);
   document.write(updatedPara);
 }
